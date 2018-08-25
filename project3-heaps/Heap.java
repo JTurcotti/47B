3a0fb52a1c15f2c1bd359e54f41996842a4aef1f
Josh Turcotti <turcotti.joshua@gmail.com>
2018-08-25 01:00:01 -0700

    complete implementation of link-based binary heap

--- project3-heaps/Heap.java
+++ project3-heaps/Heap.java
@@ -8,8 +8,21 @@ public class Heap
     private Element root = null;
     private Element last = null;
 
+    //checks if heap is empty, O(1)
+    public boolean isEmpty() {
+	return root == null;
+    }
 
-    //adds a number to the heap
+    //returns the max element, if it exists, O(1)
+    public int top() {
+	if (isEmpty()) {
+	    throw new NoSuchElementException("Heap is empty!");
+	} else {
+	    return root.value;
+	}
+    }
+    
+    //adds a number to the heap, log n (going up) + log n (going down) + log n (bubbling up) = O(log n)
     public void add(int n) {
 	if (root == null) {
 	    root = new Element(n, null);
@@ -33,11 +46,74 @@ public class Heap
 	    last = e.addLeft(n);
 	}
 	size++;
-    }	    		
+	bubbleUp(last);
+    }
 
-    /**
-     *  Display the heap.
-     */
+    //removes top value from heap and returns it, log n (going up) + log n (going down) + log n (bubbling down) = O(log n)
+    public int remove() {
+	int top = top();
+	
+	swap(root, last);
+
+	if (last == root) {
+	    root = null;
+	    last = null;
+	} else if (last.isRight()) {
+	    last.parent.right = null;
+	    last = last.parent.left;
+	} else {
+	    Element e = last;
+	    while (true) {
+		if (e == root) {
+		    break;
+		} else if (e.isRight()) {
+		    e = e.parent.left;
+		    break;
+		}
+		e = e.parent;
+	    }
+	    while (e.right != null) {
+		e = e.right;
+	    }
+	    last.parent.left = null;
+	    last = e;
+	}
+	
+	size--;
+	bubbleDown(root);
+
+	return top;
+    }	    
+	
+    //swap values with parent until heap prperty is restored, O(log n)
+    private void bubbleUp(Element e) {
+	if (e != root && e.value > e.parent.value) {
+	    swap(e, e.parent);
+	    bubbleUp(e.parent);
+	}
+    }
+
+    //swap values with max child until heap property is restored, O(log n)
+    private void bubbleDown(Element e) {
+	if (e != null && e.left != null) {
+	    if (e.right != null && e.right.value > e.left.value) {
+		swap(e, e.right);
+		bubbleDown(e.right);
+	    } else {
+		swap(e, e.left);
+		bubbleDown(e.left);
+	    }
+	}
+    }
+
+    //swap the values of two nodes, O(1)
+    private void swap(Element e, Element f) {
+	int temp = e.value;
+	e.value = f.value;
+	f.value = temp;
+    }
+    
+    //display the heap, O(n)
     public void display()
     {
         System.out.println("Heap (" + size + " elements):");
@@ -45,10 +121,6 @@ public class Heap
         System.out.println();
     }
 
-    /**
-     * Display the subheap rooted at element k, indenting each element
-     * according to the indent level argument.
-     */
     private void displayHelper(Element e, int indentLevel)
     {
 	if (e == null) return;
@@ -63,7 +135,7 @@ public class Heap
     }
 
 
-    //check if tree obeys max-heap property
+    //check if tree obeys max-heap property, O(n)
     public void check() {
 	if (root != null)
 	    root.check();
@@ -79,7 +151,7 @@ public class Heap
 	    this.parent = parent;
 	}
 
-	//check if tree rooted at this element obeys the max-heap property
+	//check if tree rooted at this element obeys the max-heap property, O(n)
 	public void check() {
 	    if (left != null) {
 		assert left.value < value: "checking left value of " + value;
@@ -91,24 +163,24 @@ public class Heap
 	    }
 	}
 
-	//checks if node is a left node
+	//checks if node is a left node, O(1)
 	public boolean isLeft() {
 	    return !(this == root) && this == parent.left;
 	}
 
-	//checks if node is a right node
+	//checks if node is a right node, O(1)
 	public boolean isRight() {
 	    return !(this == root) && this == parent.right;
 	}
 
-	//adds and returns new node to left
+	//adds and returns new node to left, O(1)
 	public Element addLeft(int n) {
 	    assert left == null: "checking if " + value + " is open to the left";
 	    left = new Element(n, this);
 	    return left;
 	}
 
-	//adds and returns new node to right
+	//adds and returns new node to right, O(1)
 	public Element addRight(int n) {
 	    assert right == null: "checking if " + value + " is open to the right";
 	    right = new Element(n, this);
@@ -119,9 +191,14 @@ public class Heap
 
     public static void main(String[] args) {
 	Heap h = new Heap();
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 5; i++) {
+	    System.out.println("Adding " + i + " to heap");
 	    h.add(i);
+	    h.display();
 	}
-	h.display();
+	for (int i = 0; i < 5; i++) {
+	    System.out.println("Removing " + h.remove() + " from heap");
+	    h.display();
+	}
     }
 }
